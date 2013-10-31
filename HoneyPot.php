cea6bdf3e28e2cb571e72accf680403b560eb231
egor zaretskiy <gorcer@gmail.com>
2013-10-31 18:28:17 +1100

    That's all folks

--- HoneyPot.php
+++ HoneyPot.php
@@ -1,23 +1,38 @@
 <?php
 
+/**
+ * HoneyPot class file.
+ *
+ * @author Egor Zaretskiy <gorcer@gmail.com>
+ * @link https://github.com/gorcer/HoneyPot
+  */
 
 class HoneyPot {
 	
-	public static function check($ip=false)
+	public $access_key = '';
+	
+	public function check($ip=false)
 	{
+		
 		if (!$ip)
 		$ip = $_SERVER['REMOTE_ADDR'];
 		
-		$access_key = '';
+		if ($this->access_key=='')
+			throw new EAuthException(Yii::t('honeypot', 'Undefined access key'), 500);
+		
+		$access_key = $this->access_key;
 		$domain = 'dnsbl.httpbl.org';
 		$dns_query = $access_key . '.' . implode ( '.', array_reverse ( explode ( '.', $ip ) ) ) . '.' . $domain;
 		
-		$dns_responce = gethostbyname ( $dns_query );
-		if ($dns_query != $dns_responce) {
-			//@file_put_contents ( ROOT . 'cache/blackip_activity.log', date ( 'Y/m/d H:i' ) . " $ip => $dns_responce\t" . getenv ( 'REQUEST_URI' ) . "\n", FILE_APPEND );
-			return explode ( '.', $dns_responce );
+		$dns_responce = @gethostbyname ( $dns_query );		
+		if ($dns_query != $dns_responce) {			
+			return false;
 		} else
-			return null;
+			return true;		
+	}
+	
+	public function init()
+	{
 		
 	}
 }
--- README.md
+++ README.md
@@ -4,7 +4,25 @@ HoneyPot
 Extension check IP in global anti-spam database http://www.projecthoneypot.org
 
 Install
-=====
+=======
 1. Register on http://www.projecthoneypot.org
 2. Go to Services - HTTP Black List - Enter capcha - get your http:BL Access Key 
 3. Copy HoneyPot path to your extension path
+4. Add to config:
+'components'=>array(
+	'honeypot' => array(
+					'class' => 'application.extensions.HoneyPot.HoneyPot',
+					'access_key'=>'YOUR-ACCESS-KEY'
+			),
+			.....
+			)
+			
+			
+Usage
+=====			
+
+$result=Yii::app()->honeypot->check($ip);
+
+Check ip in Honeypot database. Return *true* if IP didn't exists and *false* if ip found.
+To view information about blocked ip see this page http://www.projecthoneypot.org/ip_{IP} (replace {IP}).
+$ip is optional, by default it's $_SERVER['REMOTE_ADDR'].
